79ba659015cb4e6e81361f46da923e7c058969e4
tupt <thanhtu.vn.1999@gmail.com>
2022-10-05 22:25:15 +0700

    add page

--- lib/router/pages.dart
+++ lib/router/pages.dart
@@ -1,11 +1,19 @@
+import 'package:clone_app/binding/auth_binding.dart';
 import 'package:clone_app/binding/home_binding.dart';
 import 'package:clone_app/router/screen.dart';
-import 'package:clone_app/views/home.dart';
+import 'package:clone_app/views/auth_view.dart';
+import 'package:clone_app/views/home_view.dart';
 import 'package:get/route_manager.dart';
 
 class ListPage {
   static var listPages = [
-    GetPage(name: Screen.HOME, page: () => HomeView(), binding: HomeBinding()),
-    GetPage(name: Screen.AUTH, page: () => HomeView()),
+    GetPage(
+        name: Screen.HOME,
+        page: () => const HomeView(),
+        binding: HomeBinding()),
+    GetPage(
+        name: Screen.AUTH,
+        page: () => const AuthView(),
+        binding: AuthBinding()),
   ];
 }
